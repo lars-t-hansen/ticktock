6d7b8dc23a22a2be17a157a39a03b902ba1ddef1
Lars T Hansen <lth@acm.org>
2015-10-20 18:29:21 +0200

    Bugfix

--- ticktock-master.js
+++ ticktock-master.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var w = new Worker("hitme-worker.js");
+var w = new Worker("ticktock-worker.js");
 var then = 0;
 var count = 0;
 var buckets = [];
@@ -27,7 +27,7 @@ w.onmessage = function (ev) {
 	    if (a[0] < b[0]) return -1;
 	    if (a[0] > b[0]) return 1;
 	    return 0; });
-	document.getElementById("scrool").innerHTML = "<pre>" + a.map((x) => x[0] + " " + x[1]).join("\n") + "</pre>";
+	document.getElementById("scrool").innerHTML = "<pre>" + a.map(function(x) { return x[0] + " " + x[1] }).join("\n") + "</pre>";
     }
 };
 w.postMessage("go");
